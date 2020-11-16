1bd70faade3caecbb1ab20621862a846f8fc781b
Shaik Mathar <shaik.mathar99@gmail.com>
2020-11-16 23:10:39 +0530

    Add files via upload

--- Code/BERT.py
+++ Code/BERT.py
@@ -6,52 +6,67 @@ Created on Tue Nov 10 22:33:48 2020
 """
 
 from transformers import BertTokenizer, BertModel
-import logging
 import torch
-import matplotlib.pyplot as plt
 import re
-import torch
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 tokens = []
 tokenIDs = []
 segmentIDs = []
-
+hidden_states = []
 model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states = True)
 
 def InputFormatting(inputText):
     global tokens
     global tokenIDs
     global segmentIDs
-    
-    inputText = re.sub('<.*>', '', inputText)   #Removes XML tags
-    inputText = re.sub('ENDOFARTICLE.', '', inputText)  #Removes ENDOFARTICLE tag
-
-    inputText = "[CLS]" + inputText + "[SEP]"   #Adds Special tokens
+    inputText = re.sub('<.*>', '', inputText)
+    inputText = re.sub('ENDOFARTICLE.', '', inputText)
+    inputText = "[CLS]" + inputText + "[SEP]"
     tokens = tokenizer.tokenize(inputText)
     tokenIDs = tokenizer.convert_tokens_to_ids(tokens)
-    segmentIDs = [1] * len(tokens)
-    
+    segmentIDs = [1] * len(tokens)   
     return tokens, tokenIDs, segmentIDs
 
-def ExtractEmbeddings():
+def ExtractHiddenStates():
     global tokens
     global tokenIDs
     global segmentIDs
-    global model
-    
+    global model   
     tokenIDsTensor = torch.tensor([tokenIDs])
     segmentIDsTensor = torch.tensor([segmentIDs])
-    model.eval()
-    
+    model.eval()   
     with torch.no_grad():
-        output = model(tokenIDsTensor, segmentIDsTensor)
-    print(output)
+        output = model(tokenIDsTensor, segmentIDsTensor)    
+    global hidden_states
+    hidden_states = output[2]
+    return hidden_states
+    
+def CreateInputVectors():
+    h = ExtractHiddenStates()
+    token_Embeddings = torch.stack(h, dim=0)
+    token_Embeddings = torch.squeeze(token_Embeddings, dim=1)
+    token_Embeddings = token_Embeddings.permute(1, 0, 2)
+    
+    token_Vectors = []
+    for token in token_Embeddings:
+        vec = torch.cat((token[-1], token[-2], token[-3], token[-4]), dim=0)
+        token_Vectors.append(vec)
+        
+    return token_Vectors
     
 def main():
-    text = "I have so many questions; for example: will this work (Re-moval of punctuation, I mean."
-
+    text = "Test Line. I hope this works. Persnickety. Inchoate."
+    t, tID, sID = InputFormatting(text)
+    """
     print(text)
-    print(InputFormatting(text))
-
-    ExtractEmbeddings()
+    print(t)
+    print(tID)
+    print(sID)
+    """
+    Vectors = CreateInputVectors()
+    with open("inputVectors.txt", "w") as file:
+        for token in Vectors:
+            print(token, file = file)
+    
+main()
